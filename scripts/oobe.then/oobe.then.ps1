6d20a397672f64e37884c44a198a58d69733ecb6
Gee Law <geelaw@outlook.com>
2018-05-03 18:12:37 +0800

    Update oobe.then script.

--- scripts/oobe.then/oobe.then.ps1
+++ scripts/oobe.then/oobe.then.ps1
@@ -12,165 +12,164 @@
     3. Prompts the user to set a new locale;
     4. Prompts the user to change the registration information;
     5. Interactively removes user-selected Microsoft Store apps from the system;
-    6. Updates help content for PowerShell.
+    6. Sets DWM inactive window colour.
+    7. Updates help content for PowerShell.
 
     All steps can be turned off in advance with switches;
     in addition, step 2, 3, 4 and 5 can be cancelled on demand.
 
-.PARAMETER NoDisableLogonBackground
-    Skips the step to disable background on Welcome Screen.
-    Note that setting this switch does not revert the policy
-    to enable background.
+.PARAMETER DisableLogonBackground
+    Disables background image on Welcome Screen without confirmation.
 
-.PARAMETER NoRenameComputer
-    Skips the step to rename computer.
+.PARAMETER ComputerName
+    Sets a new computer name. If blank, computer name is left unchanged. If not supplied, the value is asked interactively.
 
-.PARAMETER NoSetLocale
-    Skips the step to set a new locale.
+.PARAMETER Locale
+    Sets a new locale. If blank, locale is left unchanged. If not supplied, the value is asked interactively.
 
-.PARAMETER NoRegistration
-    Skips the step to change the registration information.
+.PARAMETER RegisteredOwner
+    Sets a new registered owner. If blank, the value is left unchanged. If not supplied, the value is asked interactively. You must not supply the value as "!". To set the registered owner to "!", supply "?!".
 
-.PARAMETER NoRemovePackages
-    Skips the step to interactively select which appx packages to remove.
+.PARAMETER RegisteredOrganization
+    Sets a new registered organisation. If blank, the value is left unchanged. If not supplied, the value is asked interactively. You must not supply the value as "!". To set the registered organisation to "!", supply "?!".
 
-.PARAMETER NoUpdateHelp
-    Skips the step to update help.
+.PARAMETER SkipRemovePackages
+    Skips the step to interactively select which appx (provisioned) packages to remove.
+
+.PARAMETER SetDwmInactiveColor
+    Forces the user to choose a DWM inactive window title bar colour.
+
+.PARAMETER UpdateHelp
+    Updates help for PowerShell without confirmation.
 
 .LINK
     https://github.com/GeeLaw/PowerShellThingies/tree/master/scripts/oobe.then
 #>
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
 Param
 (
     [Parameter(Mandatory = $false)]
-    [switch]$NoDisableLogonBackground,
+    [switch]$DisableLogonBackground,
     [Parameter(Mandatory = $false)]
-    [switch]$NoRenameComputer,
+    [string]$ComputerName = '!',
     [Parameter(Mandatory = $false)]
-    [switch]$NoSetLocale,
+    [string]$Locale = '!',
     [Parameter(Mandatory = $false)]
-    [switch]$NoRegistration,
+    [string]$RegisteredOwner = '!',
     [Parameter(Mandatory = $false)]
-    [switch]$NoRemovePackages,
+    [string]$RegisteredOrganization = '!',
     [Parameter(Mandatory = $false)]
-    [switch]$NoUpdateHelp
+    [switch]$SkipRemovePackages,
+    [Parameter(Mandatory = $false)]
+    [switch]$SetDwmInactiveColor,
+    [Parameter(Mandatory = $false)]
+    [switch]$UpdateHelp
 )
 Process
 {
-    $local:ErrorActionPreference = 'Inquire';
-    $local:WarningPreference = 'Continue';
-    $local:VerbosePreference = 'Continue';
-
-    If (-not $NoDisableLogonBackground)
+    <# Disables logon background on Welcome Screen. #>
+    If ($DisableLogonBackground -or $PSCmdlet.ShouldProcess('Welcome Screen', 'Disable background image'))
     {
         Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\System' -Name 'DisableLogonBackgroundImage' -Type DWord -Value 1;
         Write-Verbose 'Disabled logon background image of Welcome Screen.';
     }
-    If (-not $NoRenameComputer)
+    Else
     {
-        $local:computerSystem = Get-WmiObject Win32_ComputerSystem;
-        $local:newName = Read-Host -Prompt "Current computer name is `"$($computerSystem.Name)`", type a new name or ignore";
-        If ($newName.Trim() -ne '')
+        Write-Verbose 'Did not disable logon background image of Welcome Screen.';
+    }
+    <# Renames computer. #>
+    If ($ComputerName -eq '!')
+    {
+        $ComputerName = Read-Host -Prompt "Current computer name is `"$((Get-WmiObject Win32_ComputerSystem).Name)`".`nType a new name (blank to ignore)";
+        $ComputerName = $ComputerName.Trim();
+    }
+    If (-not [string]::IsNullOrWhiteSpace($ComputerName))
+    {
+        $local:renameReturnValue = (Get-WmiObject Win32_ComputerSystem).Rename($ComputerName).ReturnValue;
+        $local:renameMessage = ((net.exe helpmsg $renameReturnValue) -join "`n").Trim();
+        $local:renameErrorMessage = "Renaming computer returned $renameReturnValue [$renameMessage].";
+        Write-Verbose $renameErrorMessage;
+        If ($renameReturnValue -ne 0)
         {
-            $local:retRename = $computerSystem.Rename($newName).ReturnValue;
-            $local:renameMsg = ((net.exe helpmsg $retRename) -join "`n").Trim();
-            $local:renameErr = "Renaming computer returned $retName [$renameMsg].";
-            Write-Verbose $renameErr;
-            If ($local:retRename -ne 0)
-            {
-                Write-Error -ErrorId "Win32Error_$retRename" -Message $renameErr;
-            }
+            Write-Error -ErrorId "Win32Error_$renameReturnValue" -Message $renameErrorMessage;
+        }
+    }
+    Else
+    {
+        Write-Verbose 'Did not rename computer.';
+    }
+    <# Sets a new locale. #>
+    If ($Locale -eq '!')
+    {
+        $local:currentLocale = Get-WinSystemLocale;
+        $Locale = Read-Host -Prompt "Current locale is $($currentLocale.Name) ($($currentLocale.LCID)) $($currentLocale.DisplayName).`nType a new locale (e.g., zh-CN; blank to ignore)";
+    }
+    If (-not [string]::IsNullOrWhiteSpace($Locale))
+    {
+        Set-WinSystemLocale -SystemLocale $Locale;
+        Write-Verbose "Tried setting locale to $Locale.";
+    }
+    Else
+    {
+        Write-Verbose 'Did not set a new locale.';
+    }
+    <# Set a new registered owner. #>
+    If ($RegisteredOwner -eq '!')
+    {
+        $local:currentRegisteredOwner = (Get-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').RegisteredOwner;
+        If ($currentRegisteredOwner -eq $null)
+        {
+            $currentRegisteredOwner = '(not set)';
         }
         Else
         {
-            Write-Verbose 'Did not rename computer.';
+            $currentRegisteredOwner = "`"$currentRegisteredOwner`"";
         }
+        $RegisteredOwner = Read-Host -Prompt "Current registered owner is $currentRegisteredOwner.`nTo ignore, type blank.`nNon-empty response will set RegisteredOwner.`nThe first question mark will be removed, so that `"?`" sets RegisteredOwner to `"`".`nEnter your choice";
     }
-    If (-not $NoSetLocale)
+    If (-not [string]::IsNullOrEmpty($RegisteredOwner))
     {
-        $currentLocale = Get-WinSystemLocale;
-        $newLocale = Read-Host -Prompt "Current locale is $($currentLocale.Name) ($($currentLocale.LCID)) $($currentLocale.DisplayName),`nEnter a new locale name (e.g., zh-CN; empty to leave untouched)";
-        If ($newLocale -ne '')
+        If ($RegisteredOwner.StartsWith('?'))
         {
-            Set-WinSystemLocale -SystemLocale $newLocale;
-            Write-Verbose "Tried setting locale to $newLocale.";
+            $RegisteredOwner = $RegisteredOwner.Substring(1);
+        }
+        Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' `
+            -Name 'RegisteredOwner' -Value $RegisteredOwner;
+        Write-Verbose "Set RegisteredOwner to `"$RegisteredOwner`".";
+    }
+    Else
+    {
+        Write-Verbose 'Did not set RegisteredOwner.';
+    }
+    <# Set a new registered organisation. #>
+    If ($RegisteredOrganization -eq '!')
+    {
+        $local:currentRegisteredOrganization = (Get-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').RegisteredOrganization;
+        If ($currentRegisteredOrganization -eq $null)
+        {
+            $currentRegisteredOrganization = '(not set)';
         }
         Else
         {
-            Write-Verbose "Did not change locale.";
+            $currentRegisteredOrganization = "`"$currentRegisteredOrganization`"";
         }
+        $RegisteredOrganization = Read-Host -Prompt "Current registered organisation is $currentRegisteredOrganization.`nTo ignore, type blank.`nNon-empty response will set RegisteredOrganization.`nThe first question mark will be removed, so that `"?`" sets RegisteredOrganization to `"`".`nEnter your choice";
     }
-    If (-not $NoRegistration)
+    If (-not [string]::IsNullOrEmpty($RegisteredOrganization))
     {
-        $regReg = Get-Item -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT';
-        $regReg = $regReg.OpenSubKey('CurrentVersion', $true);
-        Try
+        If ($RegisteredOrganization.StartsWith('?'))
         {
-            $regUserName = $regReg.GetValue('RegisteredOwner');
-            If ($regUserName -eq $null)
-            {
-                $regUserName = 'not set';
-            }
-            Else
-            {
-                $regUserName = '"' + $regUserName + '"';
-            }
-            $regOrg = $regReg.GetValue('RegisteredOrganization');
-            If ($regOrg -eq $null)
-            {
-                $regOrg = 'not set';
-            }
-            Else
-            {
-                $regOrg = '"' + $regOrg + '"';
-            }
-            Write-Verbose "Current registered owner is $regUserName.";
-            Write-Verbose 'To leave the setting untouched, enter nothing.';
-            Write-Verbose 'Non-empty input will set RegisteredOwner.';
-            Write-Verbose 'The first question mark will be removed, so that "?" sets it to "".';
-            $regUserName = Read-Host 'Enter your choice';
-            If ($regUserName -ne '')
-            {
-                If ($regUserName.StartsWith('?'))
-                {
-                    $regUserName = $regUserName.Substring(1);
-                }
-                $regReg.SetValue('RegisteredOwner', $regUserName, 'String');
-                Write-Verbose "Set RegisteredOwner to `"$regUserName`".";
-            }
-            Else
-            {
-                Write-Verbose 'Did not set RegisteredOwner.';
-            }
-            Write-Verbose "Current registered organisation is $regOrg.";
-            Write-Verbose 'Same syntax as that for RegisteredOwner.';
-            $regOrg = Read-Host 'Enter your choice';
-            If ($regOrg -ne '')
-            {
-                If ($regOrg.StartsWith('?'))
-                {
-                    $regOrg = $regOrg.Substring(1);
-                }
-                $regReg.SetValue('RegisteredOrganization', $regOrg, 'String');
-                $regReg.Flush();
-                Write-Verbose "Set RegisteredOrganization to `"$regOrg`".";
-            }
-            Else
-            {
-                Write-Verbose 'Did not set RegisteredOrganization.';
-            }
+            $RegisteredOrganization = $RegisteredOrganization.Substring(1);
         }
-        Catch
-        {
-            Throw;
-        }
-        Finally
-        {
-            $regReg.Close();
-        }
+        Set-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' `
+            -Name 'RegisteredOrganization' -Value $RegisteredOrganization;
+        Write-Verbose "Set RegisteredOrganization to `"$RegisteredOrganization`".";
     }
-    If (-not $NoRemovePackages)
+    Else
+    {
+        Write-Verbose 'Did not set RegisteredOrganization.';
+    }
+    If (-not $SkipRemovePackages)
     {
         $getChoice = { Param([string]$HelperFile, [UInt32]$Length, [string]$Hash)
             $lastInput = '?';
@@ -182,7 +181,7 @@ Process
                 {
                     Start-Process -FilePath $HelperFile;
                 }
-                $lastInput = Read-Host -Prompt 'Paste the summary, type "?" to open helper again, or type "??" to choose empty list';
+                $lastInput = Read-Host -Prompt "Type `"?`" to open helper again, `"??`" to choose empty list.`nPaste the summary";
                 If ($lastInput -eq '?')
                 {
                     Continue;
@@ -231,10 +230,27 @@ Process
         & $getChoice -HelperFile $tempFile -Length $provisionedPackages.Length -Hash $hash |
             ForEach-Object { Remove-AppxProvisionedPackage -Online -PackageName ($provisionedPackages[$_].PackageName); };
     }
-    If (-not $NoUpdateHelp)
+    Else
+    {
+        Write-Verbose 'Skipped the step to remove appx (provisioned) packages.';
+    }
+    If ($SetDwmInactiveColor -or $PSCmdlet.ShouldProcess('Inactive window title bar color', 'Set as 0xff66666666'))
+    {
+        Set-ItemProperty -LiteralPath 'HKCU:\SOFTWARE\Microsoft\Windows\DWM' -Name 'AccentColorInactive' -Value 0xff666666 -Type DWord;
+        Write-Verbose 'Set inactive window title bar color to 0xff666666.';
+    }
+    Else
+    {
+        Write-Verbose 'Did not set inactive window title bar color.';
+    }
+    If ($UpdateHelp -or $PSCmdlet.ShouldProcess('PowerShell help content', 'Update'))
     {
         Update-Help -Force;
         Write-Verbose 'Updated help content for PowerShell.';
+    }
+    Else
+    {
+        Write-Verbose 'Did not update help content for PowerShell.';
     }
     Write-Verbose 'Done. You might want to restart your computer.';
 }
