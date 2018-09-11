bd7db1d5bb8fb191552e62d48332a90a7999d921
Gee Law <geelaw@outlook.com>
2018-09-11 11:11:48 -0700

    Refactor WinConsole.

--- scripts/WinConsole/WinConsole.ps1
+++ scripts/WinConsole/WinConsole.ps1
@@ -1,5 +1,4 @@
 #Requires -Version 4.0
-#Requires -RunAsAdministrator
 
 <#
 .SYNOPSIS
@@ -8,34 +7,45 @@
 .LINK
     https://github.com/GeeLaw/PowerShellThingies/tree/master/scripts/WinConsole
 #>
-
-$script:WarningActionPreference = 'Inquire';
-$script:ErrorActionPreference = 'Stop';
+[CmdletBinding()]
+Param()
 
 If ($Host.Version -lt '4.0')
 {
     Write-Error 'This script requires PowerShell 4.0 or higher.';
+    Return;
 }
 Else
 {
-    Write-Warning "Install YaHei Consolas if you haven't.";
-    Write-Warning 'The script is going to try installing YaHei Consolas Hybrid to your console (if not already installed).';
+    Write-Warning "Install YaHei Consolas Hybrid if you haven't.";
 
     $script:consoleTtf = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont';
     $script:page936 = Get-ItemProperty -Path $consoleTtf | Get-Member -MemberType NoteProperty | Where-Object Name -match '0*936'
     If (($page936 | Where-Object { (Get-ItemPropertyValue -Path $consoleTtf -Name $_.Name) -eq 'YaHei Consolas Hybrid' }).Length -gt 0)
     {
-        Write-Host 'You have YaHei Consolas Hybrid in console fonts.';
+        Write-Verbose 'You have YaHei Consolas Hybrid in console fonts.';
     }
     Else
     {
-        $script:propName = '936';
-        While (($page936 | Where-Object Name -eq $propName).Length -gt 0)
+        Write-Verbose 'Installing YaHei Consolas Hybrid in elevated process.';
+        $procArgs = '-NoLogo -NoProfile -NonInteractive -WindowStyle Hidden ';
+        $procArgs += '-ExecutionPolicy Bypass -Command & "';
+        $procArgs += [System.IO.Path]::Combine($PSScriptRoot, 'Install-YaHeiConsolas.ps1');
+        $procArgs += '"';
+        $proc = Start-Process -FilePath 'powershell' -ArgumentList $procArgs `
+            -Verb 'runas' -WindowStyle 'Hidden' `
+            -WorkingDirectory $PSScriptRoot -PassThru;
+        If ($proc -eq $null)
         {
-            $propName = '0' + $propName;
+            Write-Error 'Failed to start the process to install YaHei Consolas Hybrid into the registry.';
+            Return;
         }
-        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont' -Name $propName -Value 'YaHei Consolas Hybrid';
-        Write-Host 'Added YaHei Consolas Hybrid to console fonts.';
+        $proc.WaitForExit();
+        If ($proc.ExitCode -ne 0)
+        {
+            Write-Error 'Failed to install YaHei Consolas Hybrid into the registry.';
+            Return;
+        }
     }
 
     Write-Warning 'The script is going to reset all your console default preference to default.';
@@ -95,5 +105,6 @@ Else
 
     Set-ItemProperty -Path 'HKCU:\Console' -Name 'FaceName' -Type 'String' -Value 'YaHei Consolas Hybrid';
 
-    Write-Host 'Finished configuring your console. You should recreate shortcuts of console applications you have used so that they get the default look.';
+    Write-Verbose 'Finished configuring your console.';
+    Write-Warning 'You should recreate shortcuts of console applications you have used so that they get the default look.';
 }
